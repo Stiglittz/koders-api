971002bbecf7b7e9f7baa52c7069961b07341928
Stiglitz <hgogzzs@gmail.com>
2024-07-09 17:39:28 -0600

    ...

--- src/usecases/users.usecase.js
+++ src/usecases/users.usecase.js
@@ -58,7 +58,7 @@ async function create(userData) {
 
 async function getById(id) {
     const user = await Users.findById(id)/* .populate("generation") */
-    return user
+    /* return user */
  /*
 
 
