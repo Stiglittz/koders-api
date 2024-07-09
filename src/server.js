f1687329ca7495fd62d3a0a189d2b9e35e0391c9
Stiglitz <hgogzzs@gmail.com>
2024-07-09 15:26:11 -0600

    faltaba agregar el uso de cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 
 const usersRouter = require("./routes/users.router")
 const authRouter = require("./routes/auth.router")
@@ -6,7 +7,7 @@ const postsRouter = require("./routes/posts.router")
 
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 
 app.use("/users", usersRouter)
