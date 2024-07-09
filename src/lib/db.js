f559e39253ca06d007abd0f37da8a008635b37a4
Stiglitz <hgogzzs@gmail.com>
2024-07-09 17:42:16 -0600

    ..

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
-/* const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
 
@@ -8,23 +8,4 @@ function connect () {
     return mongoose.connect(URI)
 }
 
-module.exports= { connect } */
-
-const mongoose = require("mongoose");
-
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-
-const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
-
-function connect() {
-    return mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log('Connected to the database');
-    }).catch((error) => {
-        console.error('Database connection error:', error);
-    });
-}
-
-module.exports = { connect };
+module.exports= { connect }
--- src/usecases/users.usecase.js
+++ src/usecases/users.usecase.js
@@ -1,44 +1,4 @@
-const createError = require("http-errors");
-const encrypt = require("../lib/encrypt");
-const Users = require("../models/users.model");
-
-async function create(userData) {
-    try {
-        console.log("Checking if user already exists with email:", userData.email);
-        const userFound = await Users.findOne({ email: userData.email });
-
-        if (userFound) {
-            console.log("User already exists:", userFound);
-            throw createError(409, "Email already in use");
-        }
-
-        console.log("Encrypting password for user:", userData.email);
-        userData.password = await encrypt.encrypt(userData.password);
-
-        console.log("Creating new user with data:", userData);
-        const newUser = await Users.create(userData);
-        console.log("New user created:", newUser);
-        return newUser;
-    } catch (error) {
-        console.error("Error in user creation:", error);
-        throw error;
-    }
-}
-
-async function getById(id) {
-    const user = await Users.findById(id);
-    return user;
-}
-
-module.exports = {
-    create,
-    getById,
-};
-
-
-
-
-/*const createError = require("http-errors")
+const createError = require("http-errors")
 const encrypt = require("../lib/encrypt")
 const Users = require("../models/users.model")
 
@@ -58,11 +18,11 @@ async function create(userData) {
 
 async function getById(id) {
     const user = await Users.findById(id)/* .populate("generation") */
-    /* return user */
- /*
+    return user
+}
 
 
 module.exports = {
     create,
     getById,
-} */
+}
